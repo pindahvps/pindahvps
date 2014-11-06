88807201a4b2a03e46fe5c81ead54d235e50fd1c
pindahvps@gmail.com <pindahvps@gmail.com@0bafa82c-5606-e191-9358-b07988906a2a>
2014-11-06 01:57:18 +0000

    ddd

--- recodeURLv2.js
+++ recodeURLv2.js
@@ -2,7 +2,8 @@ function url_changer(shorterurl) {
   var dloadbutton = document.getElementById('dloadbutton');
                         dloadbutton.href=shorterurl;
 var dloadimage = document.getElementById('dloadimage');
-                        dloadimage.src="https://googledrive.com/host/0BzluYHUlH7S3WEhhcnBhQ3RFMGc/";
+                        //dloadimage.src="https://googledrive.com/host/0BzluYHUlH7S3WEhhcnBhQ3RFMGc/";
+                        dloadimage.src="https://googledrive.com/host/0BzluYHUlH7S3N0JDRTI0Y2hidTQ/";
                         var loader = document.getElementById('loader');
   loader.setAttribute('style','display:none');
 }
