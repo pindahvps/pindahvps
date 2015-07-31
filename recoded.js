aa44a126d218a7d998505600e024ff135db1dcb1
pindahvps <pindahvps@gmail.com>
2015-08-01 00:57:52 +0700

    Update recoded.js

--- recoded.js
+++ recoded.js
@@ -5,6 +5,12 @@ function reloadScript(){
   $("head").append(s);
 }
 
+function changeBackground(){
+  var random = Math.round(5*Math.random());
+  var image = ["https://googledrive.com/host/0BzluYHUlH7S3Y1B1S1NKZnBwZVE/","https://googledrive.com/host/0BzluYHUlH7S3c3FsRXpPb0MwWjA/","https://googledrive.com/host/0BzluYHUlH7S3a2t3ck5uVFp3b3M/","https://googledrive.com/host/0BzluYHUlH7S3akh0dEM2V1hzZ00/","https://googledrive.com/host/0BzluYHUlH7S3bHk4b3ZPYWsxVUU/","https://googledrive.com/host/0BzluYHUlH7S3Tkw1cUo5YWRvV0U/","https://googledrive.com/host/0BzluYHUlH7S3VnkwS2kxVFRIQzQ/","https://googledrive.com/host/0BzluYHUlH7S3M2c0ZkVyVnBtLTg/","https://googledrive.com/host/0BzluYHUlH7S3US1laWhpQUwyZ28/","https://googledrive.com/host/0BzluYHUlH7S3VVhscHk0WC01UjA/","https://googledrive.com/host/0BzluYHUlH7S3TG92b3hPdjVpbDg/","https://googledrive.com/host/0BzluYHUlH7S3VmZtaUQtNUJYY0U/","https://googledrive.com/host/0BzluYHUlH7S3eHBqamdCTm51VG8/","https://googledrive.com/host/0BzluYHUlH7S3c29oMmo5WmM4UHc/"]
+  $("body").css("background-image","url()"); //edit, body must be in quotes!
+}
+
 function amountdownload(){
 var dt = new Date();
 		   var th = dt.getFullYear();
